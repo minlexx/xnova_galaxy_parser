59cf3e3371aa40b82e8b54bd5718522803832129
Alexey Min <alexey.min@gmail.com>
2016-08-05 01:20:10 +0500

    New image generator skeleton, using Pillow

---
+++ img_gen_pil.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+# Uses Python Imaging Library for (Pillow) to render galaxy maps
+# Requirements, installation:
+# pip3 install Pillow
+# on Debian Linux, requirements should be installed:
+# - compiler (build-essentials)
+# - zlib devel files (zlib1g-dev)
+# - libjpeg, libjpeg-devel
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
