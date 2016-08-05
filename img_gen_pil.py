4ef4fc231c2147f65d984606c7c5a3535ad20f45
Alexey Min <alexey.min@gmail.com>
2016-08-05 05:11:54 +0500

    Pillow galaxy map implementation

--- img_gen_pil.py
+++ img_gen_pil.py
@@ -1,15 +1,131 @@
 #!/usr/bin/python3
 
-# Uses Python Imaging Library for (Pillow) to render galaxy maps
+# Uses Python Imaging Library fork (Pillow) to render galaxy maps
 # Requirements, installation:
 # pip3 install Pillow
 # on Debian Linux, requirements should be installed:
-# - compiler (build-essentials)
+# - compiler (build-essential)
 # - zlib devel files (zlib1g-dev)
 # - libjpeg, libjpeg-devel
 
+import sys
+import logging
+import sqlite3
+
+try:
+    import PIL
+except ImportError:
+    PIL = None
+    sys.stderr.write('Cannot import Pillow module! Install Pillow:\n')
+    sys.stderr.write('#  pip3 install Pillow\n')
+    exit(1)
+
+
+import PIL.Image
+import PIL.ImageDraw
+
+
+# global XNova log message formatter
+g_xn_log_formatter = logging.Formatter('%(asctime)s %(levelname)s %(name)s %(message)s')
+
+
+# get "standard" logger for application, public api
+def get_logger(name, debug=False):
+    level = logging.INFO
+    if debug:
+        level = logging.DEBUG
+    # each module gets its own logger
+    logger = logging.getLogger(name)
+    logger.setLevel(level)
+    logger.propagate = False
+    # each module logger has its own handler attached
+    log_handler = logging.StreamHandler(stream=sys.stdout)
+    log_handler.setLevel(level)
+    # but all loggers/handlers have the same global formatter
+    log_handler.setFormatter(g_xn_log_formatter)
+    logger.addHandler(log_handler)
+    return logger
+
+
+g_logger = get_logger(__name__, debug=True)
+g_output_filename = 'galaxy_map.png'
+g_db_filename = 'galaxy5.db'
+g_db = sqlite3.connect(g_db_filename)
+
+SCALE_X = 2
+SCALE_Y = 100
+HEIGHT = 4*SCALE_Y
+WIDTH = 500*SCALE_X
+
+
+def load_galaxy_bg():
+    try:
+        img = PIL.Image.open('bg.png')
+        return img
+    except IOError:
+        g_logger.error('Failed to open bg.png!')
+        return None
+
+
+def generate_background() -> PIL.Image.Image:
+    img = PIL.Image.new('RGBA', (WIDTH, HEIGHT), color=(0, 0, 0, 255))
+    draw_galaxy_grid(img, (128, 128, 128, 255))
+    return img
+
+
+def draw_galaxy_grid(img: PIL.Image.Image, color: tuple):
+    draw = PIL.ImageDraw.Draw(img)
+    for x in [200, 400, 600, 800]:
+        draw.line([(x, 0), (x, 399)], fill=color)
+    for y in [100, 200, 300]:
+        draw.line([(0, y), (999, y)], fill=color)
+
+
+def draw_population(img: PIL.Image.Image):
+    draw = PIL.ImageDraw.Draw(img)
+    cur = g_db.cursor()
+    for x in range(0, 499):
+        for y in range(0, 4):
+            q = 'SELECT COUNT(*) FROM planets WHERE s=? AND g=?'
+            cur.execute(q, (x + 1, y + 1))
+            row = cur.fetchone()
+            num_planets = row[0]
+            fill_percent = num_planets / 15
+            cc = int(255 * fill_percent)
+            #
+            draw.rectangle([(x*SCALE_X,         HEIGHT - y*SCALE_Y),
+                            (x*SCALE_X+SCALE_X, HEIGHT - y*SCALE_Y - SCALE_Y)],
+                           fill=(cc, cc, cc, 255), outline=None)
+    cur.close()
+
+
+def draw_moons(img: PIL.Image.Image):
+    draw = PIL.ImageDraw.Draw(img)
+    q = 'SELECT g, s, p FROM planets WHERE luna_id > 0'
+    cur = g_db.cursor()
+    cur.execute(q)
+    rows = cur.fetchall()
+    for row in rows:
+        x = int(row[1]) * SCALE_X  # system
+        y = HEIGHT - int(row[0]) * SCALE_Y  # galaxy
+        y += round(SCALE_Y * (int(row[2]) / 15))  # position
+        # g_logger.debug('Row: {0}, xy: {1}, {2}'.format(row, x, y))
+        # img.putpixel((x, y), (255, 255, 0, 255))
+        draw.ellipse([(x-2, y-2), (x+2, y+2)], fill=(255, 255, 0, 128), outline=None)
+    cur.close()
+
+
 def main():
-    pass
+    img = generate_background()
+    draw_population(img)
+    draw_moons(img)
+    draw_galaxy_grid(img, (128, 128, 255, 255))
+    img.save(g_output_filename)
+    #
+    # cleanup
+    g_db.close()
 
 if __name__ == '__main__':
+    g_logger.info('Using Pillow library, version {0} (PIL {1})'.format(
+        PIL.PILLOW_VERSION, PIL.VERSION))
     main()
