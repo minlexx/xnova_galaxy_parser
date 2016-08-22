66e5d7797dd1e3adc343b518292c26f24bc79056
Alexey Min <alexey.min@gmail.com>
2016-08-22 09:53:47 +0500

    xnova_authorize: fix auth POST URL and cookies up to last update

--- xnova/xn_auth.py
+++ xnova/xn_auth.py
@@ -21,23 +21,25 @@ def xnova_authorize(xn_host, xn_login, xn_password) -> dict:
             'rememberme': 'on',
             'ajax': 'Y'
         }
-        post_url = 'https://uni5.xnova.su/index/login/?'
+        post_url = 'https://uni5.xnova.su/login/?'
 
     logger.info('Trying to authorize in XNova, url = {0} ...'.format(post_url))
 
     r = requests.post(post_url, data=postdata, allow_redirects=False)
     # print(r.content)  # empty
+    # print('----------------------------')
     # print(r.text)     # also empty
     cookies_dict = {}
     for single_cookie in r.cookies.iteritems():
         cookies_dict[single_cookie[0]] = single_cookie[1]
+    # print('cookies_dict will be:')
     # print(cookies_dict)
     if ('x_id' not in cookies_dict) and ('x_secret' not in cookies_dict) \
             and ('x_uni' not in cookies_dict):
         # uni4 auth failed, try uni5
         logger.warn('Uni4 auth failed')
         if xn_host.startswith('uni5'):
-            if ('u5_secret' not in cookies_dict) and ('u5_id' not in cookies_dict):
+            if ('PHPSESSID' not in cookies_dict) and ('session_id' not in cookies_dict):
                 logger.warn('Uni5 auth failed')
                 return None
     logger.info('Login OK')
