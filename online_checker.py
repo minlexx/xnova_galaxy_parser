5ac2435a1a9b0dfb4f50ef5a0338b7ba0040e12b
Alexey Min <alexey.min@gmail.com>
2016-09-17 15:46:21 +0500

    online_checker: change table structure, add del_watched_player method, improve tests, use separate DB file for tests, remove it after test

--- online_checker.py
+++ online_checker.py
@@ -5,6 +5,7 @@ import sqlite3
 import json
 import argparse
 import re
+import os
 import six
 import unittest
 
@@ -51,13 +52,15 @@ class OnlineDB:
             q = """
                 CREATE TABLE players_online(
                   player_id INT PRIMARY KEY, \n
+                  check_time INT, \n
                   online_time INT, \n
-                  num_planets INT \n
+                  num_planets INT, \n
+                  most_active_planet_id INT \n
                 )"""
             cur.execute(q)
             self.db.commit()
         cur.close()
-        g_logger.info('DB init complete')
+        g_logger.info('DB: init complete')
 
     def add_watched_player(self, player_id: int, player_name: str):
         cur = self.db.cursor()
@@ -69,7 +72,7 @@ class OnlineDB:
         cur.execute(q, (player_id,))
         rows = cur.fetchall()
         if len(rows) > 0:
-            g_logger.info('Player {0} #{1} is already watched.'.format(player_name, player_id))
+            g_logger.info('DB: Player {0} #{1} is already watched.'.format(player_name, player_id))
             return True
         q = """
         INSERT INTO watched_players (player_id, player_name, add_time)
@@ -78,7 +81,14 @@ class OnlineDB:
         cur.execute(q, (player_id, player_name, int(time.time())))
         self.db.commit()
         cur.close()
-        g_logger.info('Player {0} #{1} added to watched.'.format(player_name, player_id))
+        g_logger.info('DB: Player {0} #{1} added to watched.'.format(player_name, player_id))
+
+    def del_watched_player(self, player_id: int):
+        cur = self.db.cursor()
+        cur.execute('DELETE FROM players_online WHERE player_id=?', (player_id, ))
+        cur.execute('DELETE FROM watched_players WHERE player_id=?', (player_id, ))
+        self.db.commit()
+        cur.close()
 
     def get_watched_players_ids(self) -> list:
         cur = self.db.cursor()
@@ -108,38 +118,64 @@ class ODBTests(unittest.TestCase):
     def test_add_watched_player(self):
         player_name = 'DemonDV'
         p_tuple = self.gdb.find_player_by_name(player_name)
+        self.assertIsNotNone(p_tuple)
         if p_tuple is None:
             g_logger.error('Failed!')
         else:
             player_id = int(p_tuple[0])
             self.odb.add_watched_player(player_id, player_name)
-        self.assertIsNotNone(p_tuple)
+            wpids = self.odb.get_watched_players_ids()
+            self.assertIsNotNone(wpids)
+            self.assertEqual(len(wpids), 1)
 
     def test_get_watched_players(self):
         wpids = self.odb.get_watched_players_ids()
         for wpid in wpids:
-            print(wpid)
+            g_logger.info(wpid)
         self.assertIsNotNone(wpids)
 
+    def test_del_watched_player(self):
+        self.odb.del_watched_player(166)
+        wpids = self.odb.get_watched_players_ids()
+        self.assertIsNotNone(wpids)
+        self.assertEqual(len(wpids), 0)
+        g_logger.info('Del #166 OK')
+
     def setUp(self):
+        self.db_filename = 'online_checker_test.db'
+        try:
+            os.remove(self.db_filename)
+        except FileNotFoundError:
+            pass
         self.gdb = GalaxyDB()
-        self.odb = OnlineDB('online_checker.db')
+        self.odb = OnlineDB(self.db_filename)
         self.odb.check_database_tables()
 
     def tearDown(self):
         self.odb.close()
         self.gdb.close()
+        self.db_filename = 'online_checker_test.db'
+        try:
+            os.remove(self.db_filename)
+            g_logger.info('Tests: test DB file was removed.')
+        except FileNotFoundError:
+            pass
 
     def runTest(self):
         self.test_add_watched_player()
         self.test_get_watched_players()
+        self.test_del_watched_player()
 
 
-def main():
+def run_selftests():
     ts = unittest.TestSuite()
     tr = unittest.TestResult(verbosity=True)
     ts.addTest(ODBTests())
     ts.run(tr, debug=True)
+
+
+def main():
+    run_selftests()
 
 
 if __name__ == '__main__':
